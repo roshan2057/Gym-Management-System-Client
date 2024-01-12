748d1b193afdbc35edfc765690e852529efbaaaf
roshan2057 <roshankarki1276@gmail.com>
2024-01-12 22:04:11 +0545

    loding component

---
+++ src/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <>
+    
+<div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center bg-dark text-light p-3" style={{ zIndex: 1000, fontSize: '24px' }}>
+  Loading...........
+</div>
+    </>
+  )
+}
+
+export default Loading
